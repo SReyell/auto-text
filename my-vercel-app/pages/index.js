42b07ced297762a203fb716777c1cc6988592249
Sean <sreyell@me.com>
2024-06-16 10:58:24 -0700

    test1

---
+++ my-vercel-app/pages/index.js
@@ -0,0 +1,30 @@
+// pages/index.js
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const router = useRouter();
+  const { code } = router.query;
+
+  useEffect(() => {
+    if (code) {
+      // Construct the SMS link
+      const smsLink = `sms:888222&body=${encodeURIComponent(code)}`;
+
+      // Automatically redirect to the SMS link
+      window.location.href = smsLink;
+    }
+  }, [code]);
+
+  return (
+    <div>
+      <h1>Send Code via SMS</h1>
+      {code ? (
+        <p>Redirecting to SMS app...</p>
+      ) : (
+        <p>No code provided. Please use the URL with a ?code= parameter.</p>
+      )}
+    </div>
+  );
+}

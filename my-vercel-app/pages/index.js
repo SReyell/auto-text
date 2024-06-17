59b39667e47cda3d2d9902c22ac1a502cff84313
Sean <sreyell@me.com>
2024-06-16 23:11:12 -0700

    Update index.js

--- my-vercel-app/pages/index.js
+++ my-vercel-app/pages/index.js
@@ -11,9 +11,17 @@ export default function Home() {
     if (code) {
       // Construct the SMS link using the new format
       const smsLink = `sms://888222/;?&body=${encodeURIComponent(code)}`;
+      
+      // Log the SMS link for debugging
+      console.log('Attempting to open SMS link:', smsLink);
 
       // Directly set the window location to the SMS link
       window.location.href = smsLink;
+
+      // Alternative method using window.open
+      setTimeout(() => {
+        window.open(smsLink, '_self');
+      }, 500);
     }
   }, [code]);
 

21653f50ce1a3dff1caeda908e43b3ab90ef9503
Sean <sreyell@me.com>
2024-06-18 18:01:36 -0700

    1

--- my-sms-project/pages/api/send-sms.js
+++ my-sms-project/pages/api/send-sms.js
@@ -1,5 +1,7 @@
+// pages/api/send-sms.js
+
 export default function handler(req, res) {
-    const bodyText = encodeURIComponent('whatever'); // Customize the message as needed.
-    res.redirect(302, `sms://888222/;?&body=${bodyText}`);
-  }
-  
+  const { code } = req.query; // Extract 'code' from the query parameters
+  const bodyText = encodeURIComponent(code || 'default message'); // Encode and use 'code', or a default message if 'code' is not provided
+  res.redirect(302, `sms://888222/;?&body=${bodyText}`);
+}
--- my-sms-project/pages/index.js
+++ my-sms-project/pages/index.js
@@ -1,6 +1,24 @@
 // pages/index.js
 
 export default function Home() {
-    return null;  // This will render nothing, resulting in a blank page
+  // The page will not render anything since we are handling redirect server-side.
+  return null;
+}
+
+export async function getServerSideProps(context) {
+  const { query } = context;
+  const { code } = query; // Get the 'code' parameter from the query string
+
+  // If 'code' is provided, redirect to the SMS link
+  if (code) {
+    return {
+      redirect: {
+        destination: `sms://888222/;?&body=${encodeURIComponent(code)}`,
+        permanent: false,
+      }
+    };
   }
-  
+
+  // No 'code' provided, return an empty props object
+  return { props: {} };
+}
